c3ac970b17de967ee19a343f9584fcd12859b731
William Barnett <49733954+NovaAppsInc@users.noreply.github.com>
2025-05-19 13:35:29 -0400

    fix `lenient` option
    The `lenient` option always returns true, so I added the `is-valid-domain` package to validate whether or not it's a domain.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import isDomain from 'is-valid-domain';
+
 export default function isUrl(string, {lenient = false} = {}) {
 	if (typeof string !== 'string') {
 		throw new TypeError('Expected a string');
@@ -12,10 +14,7 @@ export default function isUrl(string, {lenient = false} = {}) {
 		new URL(string); // eslint-disable-line no-new
 		return true;
 	} catch {
-		if (lenient) {
-			return isUrl(`https://${string}`);
-		}
-
-		return false;
+        if (lenient && isDomain(string) === true) return true;
+        return false;
 	}
 }
